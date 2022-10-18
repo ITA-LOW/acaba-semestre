69da16d002e860a32708adea0a2cefde31bce136
Italo.Silva <italo.mtm@gmail.com>
2022-10-18 00:03:06 -0300

    desgraça de atualização

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const day = hour * 24;
 
 let count_down = new Date('12/23/2022 18:00:00').getTime();
 let x = setInterval(() => countDown(), second);
+let data_p_calc = new Date('11/16/2022 14:20:00').getTime();
+let data_p_fsc = new Date('11/14/2022 16:10:00').getTime();
+let data_p_termo = new Date('11/10/2022 16:10:00').getTime();
 
 function countDown() {
   let now = new Date(Date.now()).getTime();
   let diff = count_down - now;
+  let diff_p_calc = data_p_calc - now;
+  let diff_p_fsc = data_p_fsc - now;
+  let diff_p_termo = data_p_termo - now;
 
   document.getElementById('days').innerText = Math.floor(diff / day);
   document.getElementById('hours').innerText = Math.floor(diff % day / hour);
   document.getElementById('mins').innerText = Math.floor(diff % hour / minute);
   document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+
+  document.getElementById('days_c').innerText = Math.floor(diff_p_calc/day);
+  document.getElementById('days_f').innerText = Math.floor(diff_p_fsc/day);
+  document.getElementById('days_t').innerText = Math.floor(diff_p_termo/day);
 }
+
+
 
 function resetCountdown() {
   clearInterval(x);
